3411c10907730df7eda8fd539ac829abfcf410a3
Kuresh Kapadia <kuresh@gmail.com>
2018-09-09 11:03:24 +0530

    Added new project and git ignore file

---
+++ Source/Vegitron.Web.V1/Controllers/LocationController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Vegitron.Web.V1.Models;
+
+namespace Vegitron.Web.V1.Controllers
+{
+    public class LocationController : Controller
+    {
+        [HttpGet]
+        [Route("location/search/{areaCode}")]
+        public IActionResult Search(string areaCode)
+        {
+            var searchLocation = new SearchResultViewModel
+            {
+                AreaCode = areaCode,
+                Result = "Found Plot",
+            };
+            return View(searchLocation);
+        }
+    }
+}
---
+++ Source/Vegitron.Web.V1/Models/SearchResultViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vegitron.Web.V1.Models
+{
+    public class SearchResultViewModel
+    {
+        public string AreaCode { get; set; }
+
+        public string Result { get; set; }
+    }
+}
